9f0a106b8672ec12709245c73707db829750aa54
Jhorlodev <131390416+Jhorlodev@users.noreply.github.com>
2023-12-15 22:32:09 -0300

    page add

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://Jhorlodev.github.io/Jhorlodev/pesos2/'
 })
